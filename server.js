9f02ba7a8fb27c27a278732948a6caf462b2aab2
pcy <ridebus82@nate.com>
2023-05-20 18:03:43 +0900

    0520 gogogogo

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 import express from 'express';
+import path from 'path';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import cors from 'cors';
 const app = express();
-const port = 3000;
+
+
+app.set('port', process.env.PORT || 3000);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(morgan('combined'));
+    app.use(helmet({ contentSecurityPolicy: false, crossOriginEmbedderPolicy: false }));
+    app.use(hpp());
+} else {
+    app.use(morgan('dev'));
+}
+
+// ESM 오류 해결을 위해 __dirname, __filename 직접 변수 작성
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(cors());
+
 import { handler } from "./front/build/handler.js"
 
 app.use(handler);
 
-app.listen(port, () => {
-    console.log(`server running in port ${port}`);
+app.listen(app.get('port'), () => {
+    console.log(`server running in port ${app.get('port')}`);
 })

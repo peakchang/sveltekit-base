84eb16c0f9ae655c7393dd982e8f48fd036aedd4
parkchangyong <ridebus82@nate.com>
2023-06-05 16:14:42 +0900

    0605 gogogoswosogogo

--- front/src/routes/adm/+page.svelte
+++ front/src/routes/adm/+page.svelte
@@ -17,27 +17,70 @@
 
 	async function uploadFunction(imgBase64) {
 		const data = {};
+
+		console.log(imgBase64);
 		const imgData = imgBase64.split(',');
 		data['image'] = imgData[1];
-		console.log(data);
-		axios.post(
-			'/adm/upload',
-			{ body: JSON.stringify(data) },
-			{
-				headers: {
-					'Content-Type': 'application/json',
-					Accept: 'application/json'
-				}
+
+		const resultImage = dataURItoBlob(imgBase64);
+		console.log(resultImage);
+
+		const timestamp = new Date().getTime();
+		const fileName = `${timestamp}${Math.random().toString(36).substring(2, 11)}.webp`;
+		
+
+		let testForm = new FormData();
+		testForm.append('testval', 'alsdjflasjdfljasldifjalisdjf');
+		testForm.append('testval2', '43g34f3f45g45g4g45');
+		testForm.append('editorimg', resultImage, fileName);
+
+		let keys = testForm.keys();
+		for (const pair of keys) {
+			console.log(`name : ${pair}`);
+		}
+
+		let values = testForm.values();
+		for (const pair of values) {
+			console.log(`value : ${pair}`);
+		}
+
+		axios.post('/adm/upload', testForm, {
+			headers: {
+				'Content-Type': 'multipart/form-data'
 			}
-		);
+		});
+
+		// console.log(data);
+		// axios.post(
+		// 	'/adm/upload',
+		// 	{ body: JSON.stringify(data) },
+		// 	{
+		// 		headers: {
+		// 			'Content-Type': 'application/json',
+		// 			Accept: 'application/json'
+		// 		}
+		// 	}
+		// );
 	}
+
+	const dataURItoBlob = (dataURI) => {
+		const bytes =
+			dataURI.split(',')[0].indexOf('base64') >= 0
+				? atob(dataURI.split(',')[1])
+				: unescape(dataURI.split(',')[1]);
+		const mime = dataURI.split(',')[0].split(':')[1].split(';')[0];
+		const max = bytes.length;
+		const ia = new Uint8Array(max);
+		for (let i = 0; i < max; i++) ia[i] = bytes.charCodeAt(i);
+		return new Blob([ia], { type: mime });
+	};
 </script>
 
 <div class="mt-16 px-6" class:ml-40={!$pc_sidebar}>
 	{#if avatar}
 		<img id="avatar" src={avatar} alt="avatar" />
 	{:else}
-		<div>업로드 전~~</div>
+		<div>업로드 전~~!!!</div>
 	{/if}
 	<input
 		class="hidden"
--- front/src/routes/adm/upload/+server.js
+++ front/src/routes/adm/upload/+server.js
@@ -1,21 +1,44 @@
 import { writeFileSync } from 'fs';
 import { writeFile } from 'fs/promises';
 
+import { finished } from 'stream/promises';
+import { createRequire } from 'module';
+import { pipeline } from 'stream';
+import { promises as fs } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+import busboy from "busboy";
+
 export const POST = async ({ request, cookies }) => {
 
-    const data = await request.json()
+    console.log('POST 진입?!?!?!?');
+    // console.log(request);
 
-    console.log(data);
+    // const data = await request.json()
+    const data = await request.formData()
+    console.log(data.get('testval'));
+    console.log(data.get('editorimg'));
+    const file = data.get('editorimg');
+
+    await fs.writeFile('static/test.png', file.stream());
 
 
-    const jsonData = JSON.parse(data.body);
 
-    console.log('여기 포스트는 오고?!?!??!');
 
-    // 이게 BASE64 str
-    const file = jsonData['image'];
+    // console.table([...data.entries()])
+    // const reData = [...data.entries()];
+    // console.log(reData[2]);
+    // const fields = {};
+    // console.log(busboy);
 
-    await writeFileSync(`static/testimage.png`, file, 'base64');
+
+    // console.log(data);
+    // const jsonData = JSON.parse(data.body);
+    // console.log('여기 포스트는 오고?!?!??!');
+    // // 이게 BASE64 str
+    // const file = jsonData['image'];
+    // await writeFileSync(`static/testimage.png`, file, 'base64');
     // writeFileSync(`static/testimage.png`, file, 'base64');
 
     // writeFileSync(`static/testimage.png`, file, 'base64');
